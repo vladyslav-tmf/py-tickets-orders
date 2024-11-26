93e1b17df02a4b0bb09a3f5a2999a55651e690d7
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-26 23:05:52 +0200

    Fix syntax error in pagination settings
    - Removed unnecessary commas in page_size and page_size_query_param attributes.

--- cinema/pagination.py
+++ cinema/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class OrderViewSetPagination(PageNumberPagination):
-    page_size = 5,
-    page_size_query_param = "page_size",
+    page_size = 5
+    page_size_query_param = "page_size"
     max_page_size = 5
