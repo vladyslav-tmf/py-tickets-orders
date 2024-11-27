0419aba289ab7011c26798b417232f75fb8cc051
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-27 09:40:15 +0200

    Simplify cinema API URL path
    - Changed the cinema API URL path from "api/v1/cinema/" to "api/cinema/" to make tests pass.

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/cinema/", include("cinema.urls", namespace="cinema")),
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
     path("__debug__/", include("debug_toolbar.urls"))
 ]
